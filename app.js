9efd5faafb5b0a6454d8bf0eb4cdf95369247f98
dershomework <146546175+Hasan061910@users.noreply.github.com>
2026-03-18 22:24:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.getElementById("root").innerHTML = `
     <div style="background:rgba(255,255,255,0.12);padding:32px;border-radius:24px;text-align:center;max-width:700px;width:90%;">
       <h1 style="font-size:48px;margin:0 0 12px 0;">🎮 YKS TURNUVA</h1>
       <p style="font-size:20px;margin:0 0 20px 0;">Oyun hazır 😎</p>
-      <button onclick="alert('Sıradaki adımda gerçek oyunu koyacağız')" style="border:none;padding:14px 22px;border-radius:14px;font-size:18px;font-weight:bold;cursor:pointer;">
+      <button onclick="alert('Gerçek oyun birazdan geliyor')" style="border:none;padding:14px 22px;border-radius:14px;font-size:18px;font-weight:bold;cursor:pointer;">
         Başla
       </button>
     </div>
